4ecb23c0ed400ae664b21c1acbaea075b9615f08
Mark Evans <mark.evans@teamsnap.com>
2016-05-15 19:18:49 -0600

    Adds specs for Belief class

--- test/com/agnotheist/model/domain/BeliefTest.java
+++ test/com/agnotheist/model/domain/BeliefTest.java
@@ -7,8 +7,29 @@ import org.junit.Test;
 public class BeliefTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testValidate() {
+		Belief belief = new Belief ("Catholic", "Killing is bad.", User, Comment);
+				
+		assertTrue("belief validates", belief.validate());
+			System.out.println("testValidate PASSED");
+	}
+	
+	@Test
+	public void testEqualsBelief() {
+		Belief belief1 = new Belief ("Catholic", "Killing is bad.", User, Comment);
+		Belief belief2 = new Belief ("Catholic", "Killing is bad.", User, Comment);
+				
+		assertTrue("belief1 equals belief2", belief1.equals(belief2));
+			System.out.println("testEqualsComment PASSED");
+	}
+	
+	@Test
+	public void testNotEqualsUser() {
+		Belief belief1 = new Belief ("Catholic", "Killing is bad.", User, Comment);
+		Belief belief2 = new Belief ("Catholic", "Killing is kind of bad.", User, Comment);
+				
+		assertTrue("belief1 does NOT equal belief2", belief1.equals(belief2));
+			System.out.println("testNotEqualsBelief PASSED");
 	}
 
 }
