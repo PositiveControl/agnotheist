166bd1436d923c6c2ef542e0d12c4f49f0174d89
Mark Evans <mark.evans@teamsnap.com>
2016-05-15 18:35:48 -0600

    implements Comment class

--- src/com/agnotheist/model/domain/Comment.java
+++ src/com/agnotheist/model/domain/Comment.java
@@ -1,5 +1,81 @@
 package com.agnotheist.model.domain;
 
 public class Comment {
-
+	private User user;
+	private Belief belief;
+	private String userComment;
+	
+	public Comment(User user, Belief belief, String userComment) {
+		this.user = user;
+		this.belief = belief;
+		this.userComment = userComment;
+	}
+	
+	public User getUser() { return user; }
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	public Belief getBelief() { return belief; }
+	public void setBelief(Belief belief) {
+		this.belief = belief;
+	}
+	
+	public String getUserComment() { return userComment; }
+	public void setUserComment(String userComment) {
+		this.userComment = userComment;
+	}
+	
+	public boolean validate() {
+		if (user == null) return false;
+		if (belief == null) return false;
+		if (userComment == null) return false;
+		
+		return true;
+	}
+	
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) return true;
+		if (object == null) return false;
+		if (getClass() != object.getClass()) return false;
+		
+		Comment otherComment = (Comment) object;
+		
+		if (userComment == null && otherComment.userComment != null)
+			return false;
+		else if (!userComment.equals(otherComment.userComment))
+			return false;
+		if (belief == null && otherComment.belief != null)
+			return false;
+		else if (!belief.equals(otherComment.belief))
+			return false;
+		if (user == null && otherComment.user != null)
+			return false;
+		else if (!user.equals(otherComment.user))
+			return false;
+		
+		return true;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 97;
+		int result = 1;
+		
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		result = prime * result + 
+				((belief == null) ? 0 : belief.hashCode());
+		result = prime * result + 
+				((userComment == null) ? 0 : userComment.hashCode());
+		
+		return result;
+		
+	}
+	
+	@Override
+	public String toString() {
+		return "Comment [belief=" + belief + ", " + "userComment=" 
+				+ userComment + "user=" + user + "]";
+	}
 }
