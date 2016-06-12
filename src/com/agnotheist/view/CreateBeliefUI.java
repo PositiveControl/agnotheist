20e286aab87c55f78f9e99749acab0c3b0c77645
Mark Evans <mark.evans@teamsnap.com>
2016-06-12 16:52:46 -0600

    Refactors CreateBeliefUI class for better comprehension

--- src/com/agnotheist/view/CreateBeliefUI.java
+++ src/com/agnotheist/view/CreateBeliefUI.java
@@ -20,7 +20,7 @@ public class CreateBeliefUI extends JFrame implements ActionListener {
 	private static final long serialVersionUID = 3444051794458232490L;
 
 	/**
-	 * UI Components seperated by grouping
+	 * UI data components separated by grouping
 	 */
 	private JLabel beliefLabel = new JLabel("Belief");
 	private JTextField beliefText = new JTextField(20);
@@ -33,11 +33,41 @@ public class CreateBeliefUI extends JFrame implements ActionListener {
 	
 	/**
 	 * 
-	 * @param name
+	 * @param String name
 	 */
 	public CreateBeliefUI (String name) {
 		super(name);
+	}
+	
+	/**
+	 * 
+	 * @param String args
+	 */
+	public static void main (String[] args) {
+		CreateBeliefUI cb = new CreateBeliefUI("CreateBeleif");
+		cb.run();
+	}
+	
+	/**
+	 * Sets up UI and adds event listener for Create button
+	 */
+	public void run() {
+		// creating the layout by adding components to the container
+		Container rootContainer = getContentPane();
+		BorderLayout layout = new BorderLayout(5,5);
 		
+		rootContainer.setLayout(layout);
+		rootContainer.add(new JLabel("Create New Belief"), BorderLayout.NORTH);
+		rootContainer.add(beliefLabel, BorderLayout.WEST);
+		rootContainer.add(beliefText, BorderLayout.WEST);
+		
+		rootContainer.add(statementLabel, BorderLayout.EAST);
+		rootContainer.add(statementText, BorderLayout.EAST);
+		
+		rootContainer.add(cancelButton, BorderLayout.SOUTH);
+		rootContainer.add(createButton, BorderLayout.SOUTH);
+		
+		// Adding an action for when the create button is pressed
 		createButton.addActionListener(
 				new ActionListener () {
 					public void actionPerformed (ActionEvent event) 
@@ -45,7 +75,7 @@ public class CreateBeliefUI extends JFrame implements ActionListener {
 						Belief belief = new Belief();
 						belief.setReligion(beliefText.getText());
 						belief.setBeliefStatement(statementText.getText());
-						belief.setUser(null);
+						belief.setUser(null); // Need to implement create user
 						BeliefMgr beliefMgr = new BeliefMgr();
 						beliefMgr.create(
 								"CreateBelief", 
@@ -58,25 +88,11 @@ public class CreateBeliefUI extends JFrame implements ActionListener {
 				}
 			);
 		
-		Container rootContainer = getContentPane();
-		BorderLayout layout = new BorderLayout(5,5);
-		
-		rootContainer.setLayout(layout);
-		rootContainer.add(new JLabel("Create New Belief"), BorderLayout.NORTH);
-		rootContainer.add(beliefLabel, BorderLayout.WEST);
-		rootContainer.add(beliefText, BorderLayout.WEST);
-		
-		rootContainer.add(statementLabel, BorderLayout.EAST);
-		rootContainer.add(statementLabel, BorderLayout.EAST);
-		
-		rootContainer.add(cancelButton, BorderLayout.SOUTH);
-		rootContainer.add(createButton, BorderLayout.SOUTH);
-		
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		pack();
 		setVisible(true);
 	}
-
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
